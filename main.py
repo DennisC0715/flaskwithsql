8ae73384bb26dfa0364a73457d5c8bf0088b785e
Suyuan Chen <chensuyuan1989@hotmail.com>
2023-01-10 12:49:47 -0500

    second

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from flask import *
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
 app = Flask(__name__)
-all_books = []
+# db = sqlite3.connect("books-collection.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+# with app.app_context():
+#     db.create_all()
+#     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
+
+with app.app_context():
+    all_books = session.query(Book).all()
 
 @app.route('/')
 def home():
