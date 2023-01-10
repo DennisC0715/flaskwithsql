f544bbec687d719b7f38cf872b5a483767b9558a
Suyuan Chen <chensuyuan1989@hotmail.com>
2023-01-10 14:41:45 -0500

    finished project

--- main.py
+++ main.py
@@ -18,34 +18,71 @@ class Book(db.Model):
     author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
-    def __repr__(self):
-        return f'<Book {self.title}>'
+    # def __repr__(self):
+    #     return f'<Book {self.title}>'
 
 
-# with app.app_context():
-#     db.create_all()
+
+
+with app.app_context():
+    db.create_all()
+
+
+
 #     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
 #     db.session.add(new_book)
 #     db.session.commit()
 
-with app.app_context():
-    all_books = session.query(Book).all()
+# with app.app_context():
+#     book_id = 1
+#     book_to_delete = Book.query.get(book_id)
+#     db.session.delete(book_to_delete)
+#     db.session.commit()
 
 @app.route('/')
 def home():
+    with app.app_context():
+        all_books = db.session.query(Book).all()
     return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {"title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]}
-        all_books.append(new_book)
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        with app.app_context():
+            db.session.add(new_book)
+            db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
 
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    with app.app_context():
+        if request.method == "POST":
+            book_id = request.form["id"]
+            book_to_update = Book.query.get(book_id)
+            book_to_update.rating = request.form["rating"]
+            db.session.commit()
+            return redirect(url_for('home'))
+        book_id = request.args.get("id")
+        selectedBook = Book.query.get(book_id)
+    return render_template("edit.html", book=selectedBook)
+
+
+@app.route("/delete")
+def delete():
+    with app.app_context():
+        book_id = request.args.get('id')
+        book_to_delete = Book.query.get(book_id)
+        db.session.delete(book_to_delete)
+        db.session.commit()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
